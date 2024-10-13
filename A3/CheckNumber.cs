cd72e6510b6c10057a7655388d60291ee2f6aa1a
AngelMoreno24 <angel.moreno8424@gmail.com>
2024-10-12 19:36:30 -0700

    a

---
+++ A3/CheckNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace A3
+{
+    public sealed class CheckNumber : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> Text { get; set; }
+        public InArgument<string> SecretNumber { get; set; }
+
+
+        //define an output argument
+        public OutArgument<int> check { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text and SecretNumber input argument
+            string guess = context.GetValue(this.Text);
+
+            string secretNumber = context.GetValue(this.SecretNumber);
+
+
+            //sets up url for REST service with user input
+            string URL = "http://localhost:60108/Service1.svc/checkNumber?userNum=" + guess+"&SecretNum="+secretNumber;
+
+            // create a channel
+            WebClient channel = new WebClient();
+
+            // return byte array
+            byte[] abc = channel.DownloadData(URL);
+
+            // convert to mem stream
+            Stream strm = new MemoryStream(abc);
+            DataContractSerializer obj = new DataContractSerializer(typeof(string));
+
+            // convent to string
+            string randString = obj.ReadObject(strm).ToString();
+
+            //print guess result to console
+            Console.WriteLine("Your Guess is "+ randString);
+
+            //changes value of check in the workflow to stop while loop for guessing once guess is correct
+            if (randString=="correct")
+            {
+            context.SetValue(this.check, 1);
+            }
+        }
+    }
+}
---
+++ CheckNumber/Service1.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CheckNumber
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        //checks if the guess is correct
+        public string checkNumber(int userNum, int SecretNum)
+        {
+            if (userNum == SecretNum)
+                return "correct";
+            else
+                if (userNum > SecretNum)
+                return "too big";
+            else return "too small";
+        }
+    }
+}
